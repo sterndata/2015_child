76e700ce71b1006ebf169e08f8165b62711594dc
Steven Stern <webmaster@sterndata.com>
2015-02-02 13:25:42 -0600

    carousel styling

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'jetpack', get_stylesheet_directory_uri() . '/my-jetpack-carousel.css','jetpack' );
 }
 
 function load_fonts() {
