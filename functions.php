6393239699069a37c52df0513147a5094072a678
Steven Stern <webmaster@sterndata.com>
2015-06-05 10:48:04 -0500

    https

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ function theme_enqueue_styles() {
 }
 
 function load_fonts() {
-  wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?family=Bitter:400,700,400italic|Raleway:400,600');
+  wp_register_style('googleFonts', 'https://fonts.googleapis.com/css?family=Bitter:400,700,400italic|Raleway:400,600');
   wp_enqueue_style( 'googleFonts');
         }
 
